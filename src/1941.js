4a9a162ee58842c7759390dcefdaddf0b5e9d5f6
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:35:31 +0500

    Create 1941.js

---
+++ src/1941.js
@@ -0,0 +1,19 @@
+var areOccurrencesEqual = function(s) {
+    const a = s.split('');
+let obj={};
+a.forEach((item) => {
+  if(obj[item] === undefined){
+    obj[item] = 1
+  } else {
+    obj[item] = obj[item] + 1
+  }
+})
+const count = obj[s[0]]
+
+for (let value in obj) {
+  if(count !== obj[value]){
+    return false
+  }
+}
+    return true
+};
