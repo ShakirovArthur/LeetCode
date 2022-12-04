420c303176ef8ae0ad69e9d91e3ee07267b93b8a
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:20:57 +0500

    Create 771.js

---
+++ src/771.js
@@ -0,0 +1,13 @@
+var numJewelsInStones = function(jewels, stones) {
+    let arr = jewels.split('');
+let arr2 = stones.split('');
+let sum = 0;
+for (let i of arr) {
+  for(let k of arr2) {
+    if (i === k) {
+        sum = sum + 1;
+    }
+  }
+}
+    return sum;
+};
