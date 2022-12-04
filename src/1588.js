14a605b9e0f5460f0f1e0c2251a3ecd174b5aaef
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:29:16 +0500

    Create 1588.js

---
+++ src/1588.js
@@ -0,0 +1,13 @@
+var sumOddLengthSubarrays = function(arr) {
+    let count = 0;
+for(let i = 0;i < arr.length;i++){
+  const arr2=[];
+  for(let j=i;j<arr.length;j++){
+    arr2.push(arr[j])
+    if(arr2.length % 2 !== 0){
+      count = count + arr2.reduce((a,b) => a + b)
+    }
+  }
+}
+return count
+};
