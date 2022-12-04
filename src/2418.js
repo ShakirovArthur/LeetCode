3d37e3cd7b893ae20c8d3d20a0d2a32972fedd71
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:32:58 +0500

    Create 2418.js

---
+++ src/2418.js
@@ -0,0 +1,17 @@
+var sortPeople = function(names, heights) {
+   const res = [];
+for (let i = 0; i < names.length; i++) {
+  res.push([names[i],heights[i]]);
+}
+
+res.sort(function(a,b) {
+  if(b[1] < a[1]){
+    return -1
+  }
+  if(b[1] > a[1]) {
+    return 1
+  }
+})
+
+return res.map((item) => item[0]) 
+};
