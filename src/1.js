53c81904b3078efdfcf04904cde83481b5212dfc
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:43:12 +0500

    Create 1.js

---
+++ src/1.js
@@ -0,0 +1,18 @@
+var twoSum = function(nums, target) {
+  const arr = [];
+let sum = 0
+for(let i = 0;i < nums.length;i++){
+    sum = nums[i]
+    
+   for(let j = i+1;j < nums.length;j++){
+       sum = nums[i] + nums[j];
+       
+      if(sum === target) {
+        arr.push(i,j);
+        
+      }
+   }
+    
+}
+    return arr
+};
