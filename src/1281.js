56c4f496568093fe7659318d193256175e5346f0
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:23:12 +0500

    Create 1281.js

---
+++ src/1281.js
@@ -0,0 +1,18 @@
+var subtractProductAndSum = function(n) {
+    let str = String(n);
+let arr = str.split("");
+let sum = 0;
+let product = 1;
+let result = 0;
+arr.forEach(function(item){
+  product = product * Number(item);
+  
+});
+
+arr.forEach(function(item, i){
+  sum = sum + Number(item);
+  
+});
+result = product - sum;
+    return result;
+};
