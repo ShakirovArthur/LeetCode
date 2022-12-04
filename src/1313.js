f262d9d4d6fc24e2adcb94cd2d1bb693c319f988
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-04 12:25:16 +0500

    Create 1313.js

---
+++ src/1313.js
@@ -0,0 +1,9 @@
+var decompressRLElist = function(nums) {
+    let out = [];
+for (let i = 0; i < nums.length; i = i + 2) {
+  for (let j = 0; j < nums[i]; j++){
+    out.push(nums[i+1]);
+  }
+}
+    return out;
+};
