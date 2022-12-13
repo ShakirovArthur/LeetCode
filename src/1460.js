fdd232daaaf29167e6ba0367523b0d661ecbf115
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-13 16:01:18 +0500

    Create 1460.js

---
+++ src/1460.js
@@ -0,0 +1,10 @@
+var canBeEqual = function(target, arr) {
+    target.sort()
+    arr.sort()
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i] !== target[i]){
+            return false
+        }
+    }
+    return true
+};
