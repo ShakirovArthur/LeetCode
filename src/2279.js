ab81c11ac8425049e146fd94705040c0eddecd09
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-16 16:09:30 +0500

    Create 2279.js

---
+++ src/2279.js
@@ -0,0 +1,11 @@
+var percentageLetter = function(s, letter) {
+    const array = s.split('');
+    let count = 0;
+    array.forEach((item) => {
+        if(item === letter){
+            count++
+        }
+    })
+    const result = count / s.length * 100;
+    return Math.trunc(result)
+};
