8fadf6fc6c7636605b38ceda32c4273b00ec66a3
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2022-12-19 22:46:09 +0500

    Create 2341.js

---
+++ src/2341.js
@@ -0,0 +1,17 @@
+var numberOfPairs = function(nums) {
+const sortNums = nums.sort();
+let count = 0;
+let result = [];
+for (let i = 0; i < sortNums.length;i++){
+  if(sortNums[i] === sortNums[i+1]){
+    count++;
+    sortNums.splice(i,2)
+    i -= 1
+  }else {
+    result.push(sortNums[i]);
+    sortNums.shift()
+    i -=1
+  }
+} 
+return [count,result.length]   
+};
