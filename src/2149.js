ae6bae15260d8ad93f0560acbe6815fb91dac7e8
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2023-01-13 15:01:02 +0500

    Create 2149.js

---
+++ src/2149.js
@@ -0,0 +1,10 @@
+var rearrangeArray = function(nums) {
+    const pos = nums.filter((x) => x >=0)
+    const neg = nums.filter((x) => x < 0);
+    const res = [];
+
+    for (let i = 0; i < nums.length /2; i++){
+        res.push(pos[i],neg[i])
+    }
+    return res
+};
