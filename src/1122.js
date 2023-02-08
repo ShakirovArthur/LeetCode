614e9b84e53093a312589d72e5d6823f53442a4f
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2023-02-08 15:05:23 +0500

    Create 1122.js

---
+++ src/1122.js
@@ -0,0 +1,21 @@
+var relativeSortArray = function(arr1, arr2) {
+    let matches = [];
+    let noMatches = [];
+
+    arr1.forEach((item) => {
+        if(arr2.includes(item)){
+            matches.push(item)
+        } else {
+            noMatches.push(item)
+        }
+    })
+
+    matches.sort((a,b) => arr2.indexOf(a) - arr2.indexOf(b));
+
+    noMatches.sort((a,b) => a - b);
+
+    const result = matches.concat(noMatches);
+
+    return result
+
+};
