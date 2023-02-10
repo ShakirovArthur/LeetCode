8f8d8d115a686135cb789e9438a77cf8e69d5300
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2023-02-10 16:14:10 +0500

    Create 1748.js

---
+++ src/1748.js
@@ -0,0 +1,17 @@
+var sumOfUnique = function(nums) {
+    let obj = {};
+    nums.forEach((item) => {
+        if(obj[item]){
+            obj[item]++
+        }else {
+            obj[item] = 1
+        }
+    })
+    let result = 0;
+    for(let key in obj){
+        if(obj[key] === 1){
+            result += Number(key)
+        }
+    }
+    return result
+};
