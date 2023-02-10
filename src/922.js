0904afb82918998c5b14439dfd0dfd572f93e33d
Artur Shakirov <88730967+ShakirovArthur@users.noreply.github.com>
2023-02-10 10:09:38 +0500

    Create 922.js

---
+++ src/922.js
@@ -0,0 +1,21 @@
+var sortArrayByParityII = function(nums) {
+    const odd = [];
+    const even = [];
+
+    nums.forEach((item) => {
+        if(item % 2 === 0){
+            even.push(item)
+        } else {
+            odd.push(item)
+        }
+    })
+
+    const result = [];
+    
+    while(odd.length > 0 && even.length > 0){
+        result.push(even.shift());
+        result.push(odd.shift())
+    }
+
+    return result
+};
